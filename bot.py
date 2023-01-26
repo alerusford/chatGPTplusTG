0561aa096ee548560c577fc4a2d50516630cede4
alexandr <alerusford@gmail.com>
2023-01-26 19:18:18 +0300

    + генерация и модификация фоток от того же чата

--- bot.py
+++ bot.py
@@ -6,18 +6,53 @@ from telebot import types
 import json
 import traceback
 import time
+import os
+from io import BytesIO
+from PIL import Image
+from datetime import datetime
+
 
 openai.api_key = config.openai_apikey
 bot = telebot.TeleBot(config.telegram_apikey)
 
 bot.send_message('1077463086', 'i`m online 🫡')
+thisFile = os.path.abspath(__file__)
+
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, 'Привет! Я бот, который поможет тебе использовать OpenAI API для генерации текста.')
+    bot.send_message(message.chat.id, 'Привет! Я бот.')
+
+
+@bot.message_handler(commands=['pic', 'картинка', 'фото', 'фотки', 'картинки', 'pics', 'photo', 'photos'])
+def generator_pics(message):
+    input = message.text.split()
+    find_word = input[1:]
+    print(' - user: ', message.from_user.username, ', ищет: ', find_word)
+    time_start = datetime.now()
+
+    response = openai.Image.create(
+        prompt=str(find_word),
+        n=3,
+        size="1024x1024"
+    )
+    print('\n- response: ', response, '\n')
+    time_end = datetime.now()
+    time_request = time_start-time_end
+    print(' - время запроса:', time_request)
+    # image_url = response['data'][0]['url']
+
+    for url in response['data']:
+        link_photo = url['url']
+        # img = open(link_photo, 'rb')
+        bot.send_photo(message.chat.id, link_photo)
+        # image_url = response['data']['url']
+        # bot.send_message(message.chat.id, text=image_url)
+
 
 @bot.message_handler(func=lambda _: True)
 def handle_message(message):
+    time_start = datetime.now()
     # if message.from_user.username in config.users:
     response = openai.Completion.create(
         model='text-davinci-003',
@@ -31,10 +66,56 @@ def handle_message(message):
 
     print('\n - user: ', message.from_user.username)
     print(' - question: ', message.text)
-    print(' - response: ', response)
+    print(' - response: ', response['choices'][0]['text'])
+    time_end = datetime.now()
+    time_request = time_start-time_end
+    print(' - время запроса:', time_request)
     bot.send_message(chat_id=message.from_user.id, text=f"{response['choices'][0]['text']}")
     # else:
     #     bot.send_message(chat_id=message.from_user.id, text=f" - у {message.from_user.username} нет доступа!")
+
+
+@bot.message_handler(content_types=['document', 'photo'])
+def document_and_photo(message):
+    print(message.content_type)
+    if message.content_type == 'document':
+        fileID = message.document.file_id
+    elif message.content_type == 'photo':
+        fileID = message.photo[-1].file_id
+        # print(fileID)
+
+    print('fileID =', fileID)
+    file_info = bot.get_file(fileID)
+    print('file.file_path =', file_info.file_path)
+    downloaded_file = bot.download_file(file_info.file_path)
+
+    with open('temp/photo.png', 'wb') as new_file:
+        new_file.write(downloaded_file)
+
+    # Read the image file from disk and resize it
+    image = Image.open("temp/photo.png")
+    width, height = 256, 256
+    image = image.resize((width, height))
+
+    # Convert the image to a BytesIO object
+    byte_stream = BytesIO()
+    image.save(byte_stream, format='PNG')
+    byte_array = byte_stream.getvalue()
+
+    try:
+        response = openai.Image.create_variation(
+            image=byte_array,
+            n=3,
+            size="1024x1024"
+        )
+        for url in response['data']:
+            link_photo = url['url']
+            print(link_photo)
+            bot.send_photo(message.chat.id, link_photo)
+    except openai.error.OpenAIError as e:
+        print(e.http_status)
+        print(e.error)
+
 
 
 def telegram_polling():
---
+++ config_example.py
@@ -0,0 +1,4 @@
+openai_apikey = ''
+telegram_apikey = ''
+users = ''
+file_error_log = ''
